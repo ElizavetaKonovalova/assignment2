02a876a8457deeb66400293867e15506f6505443
Kevin Chun <kevinchun805@gmail.com>
2016-10-28 01:04:45 +1000

    update some css in the graph

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -21,6 +21,8 @@ socket.on('received data', function(msg){
 			case "negative":
 				data[2].number++;
 				break;
+			default:
+				break;
 		}
 		ReDraw();
 	}
@@ -39,48 +41,51 @@ function Refresh_Data(){
 }
 
 function Search(){
-	search = true;
-	Refresh_Data();
-	$("#graph").empty();
-
-	//Remove the animation
-	$('.twitter_animation').empty();
-	$('.twitter_animation').remove();
-
 	var search_key = $("#search_key").val();
-	socket.emit('search', search_key);
 	console.log(search_key);
 
-	// $("#search_key").val("");
-	
+	if(search_key){
+		search = true;
+		Refresh_Data();
+		$("#graph").empty();
 
-	//Put JSON data into the graph function
-	DrawGraph(data);
+		//Remove the animation
+		$('.twitter_animation').empty();
+		$('.twitter_animation').remove();
+
+		
+		socket.emit('search', search_key);
+		
+		// $("#search_key").val("");
+
+		//Put JSON data into the graph function
+		DrawGraph(data);
+	}
 }
 
-	var margin = {top: 40, right: 20, bottom: 50, left: 40},
+var margin = {top: 40, right: 20, bottom: 50, left: 40},
 	width = 960 - margin.left - margin.right,
 	height = 500 - margin.top - margin.bottom;
 
-	// var formatPercent = d3.format(".0%");
-	var formatPercent = d3.format("s");
+// var formatPercent = d3.format(".0%");
+var formatPercent = d3.format("s");
 
-	var x = d3.scale.ordinal()
-			.rangeRoundBands([0, width], .1);
+var x = d3.scale.ordinal()
+		.rangeRoundBands([0, width], .1);
 
-	var y = d3.scale.linear()
-			.range([height, 0]);
+var y = d3.scale.linear()
+		.range([height, 0]);
 
-	var xAxis = d3.svg.axis()
-				.scale(x)
-				.orient("bottom");
+var xAxis = d3.svg.axis()
+			.scale(x)
+			.orient("bottom");
 
-	var yAxis = d3.svg.axis()
-				.scale(y)
-				.orient("left")
-				.tickFormat(formatPercent);
+var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left")
+			.tickFormat(formatPercent);
 
-	var svg;
+var svg;
 
 function ReDraw(){
 
@@ -134,12 +139,11 @@ function DrawGraph(data){
 				.attr('class', 'd3-tip')
 				.offset([-10, 0])
 				.html(function(d) {
-					return "<strong>Frequency:</strong> <span style='color:red'>" + d.number + "</span>";
+					return "<strong>Number:</strong> <span style='color:red'>" + d.number + "</span>";
 				});
 
 	svg.call(tip);
 
-	
 	// The following code was contained in the callback function.
 	x.domain(data.map(function(d) { return d.item; }));
 	y.domain([0, d3.max(data, function(d) { return d.number; })]);
@@ -147,16 +151,19 @@ function DrawGraph(data){
 	svg.append("g")
 		.attr("class", "x axis")
 		.attr("transform", "translate(0," + height + ")")
+		.style("fill", "white")
 		.call(xAxis);
 
 	svg.append("text")      // text label for the x axis
 		.attr("x", (width/2) )
 		.attr("y",  height + 40 )
 		.style("text-anchor", "middle")
-		.text("Analyse");    
+		.style("fill", "white")
+		.text("Analyse Sentiment");    
 
 	svg.append("g")
 		.attr("class", "y axis")
+		.style("fill", "white")
 		.call(yAxis)
 	.append("text")
 		// .attr("transform", "rotate(-90)")
